5f259b33466fcc5915e7767624e1bca22c388333
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-03-03 14:27:06 +0600

    add redux thunk to middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import rootReducer from "./reducers/rootReducer";
 import cartCounter from "./middlewares/cartCounter";
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(cartCounter))
+  composeWithDevTools(applyMiddleware(cartCounter, thunk, logger))
 );
 
 export default store;
